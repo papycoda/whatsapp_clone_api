8d4269d3838bc7fabfb8a0031d94d11d4666935a
papycoda <opeyemi655@gmail.com>
2023-12-02 19:16:07 +0100

    consumers created

--- ChatAPI/chat/models.py
+++ ChatAPI/chat/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from users.models import MyUser as User
 
 class Conversation(models.Model):
     initiator = models.ForeignKey(
@@ -15,6 +16,27 @@ class Conversation(models.Model):
         related_name="conversations_participated"
     )
     start_time = models.DateTimeField(auto_now_add=True)
+
+class Room(models.Model):
+    name = models.CharField(max_length=128)
+    online = models.ManyToManyField(to=User, related_name='online_rooms', blank=True)
+
+    def get_online_count(self):
+        return self.online.count()
+
+    def join(self, user):
+        self.online.add(user)
+        self.save()
+
+    def leave(self, user):
+        self.online.remove(user)
+        self.save()
+
+    def is_user_online(self, user):
+        return user in self.online.all()
+
+    def __str__(self):
+        return f'{self.name} ({self.get_online_count()})'
 
 class Message(models.Model):
     sender = models.ForeignKey(
